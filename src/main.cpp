a3cb88a1302efd2cd4efb8045a8c6732330e4bf8
tiger <s.garmash@softerra.com>
2020-07-03 11:14:05 +0300

    Added sensebender sensors processing

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,16 @@
 
 #include <MySensors.h>
 #include <VoltageReference.h>
+#include <SI7021.h>
 
-#define SKETCH_NAME "Battery Sensor"
+#define SKETCH_NAME "Sensebender Micro"
 #define SKETCH_MAJOR_VER "0"
 #define SKETCH_MINOR_VER "7"
 
 // Sensors' Child IDs
 #define CHILD_ID_BATT 0
+#define CHILD_ID_TEMP 1
+#define CHILD_ID_HUM  2
 
 #ifdef MY_DEBUG
 unsigned long SLEEP_TIME = 10 * 1000L;  // 10s
@@ -25,7 +28,7 @@ unsigned long SLEEP_TIME = 10*60*1000; // 10min,  h*min*sec*1000
 #endif
 
 // #define FAKE_VCC uncomment to enable VCC "deviations"
-#define VCC_CALIBRATION 1128380 // determined by voltage_calibration project
+#define VCC_CALIBRATION 1097820 // determined by voltage_calibration project
 VoltageReference vRef;
 
 // Globals
@@ -33,6 +36,11 @@ float oldBatPercentage;
 int unusedPins[] = {2, 3, 4, 5, 6, 7, 8};
 // MySensors messages
 MyMessage msgBatt(CHILD_ID_BATT, V_VOLTAGE);
+MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
+MyMessage msgHum(CHILD_ID_HUM, V_HUM);
+
+#define ATSHA204_PIN   17 // A3
+SI7021 siSensor;
 
 /*
  * MySensors 2.x presentation
@@ -43,6 +51,8 @@ void presentation() {
 #endif
   sendSketchInfo(SKETCH_NAME, SKETCH_MAJOR_VER "." SKETCH_MINOR_VER);
   present(CHILD_ID_BATT, S_MULTIMETER, "Battery Voltage");
+  present(CHILD_ID_TEMP, S_TEMP);
+  present(CHILD_ID_HUM, S_HUM);
 }
 
 /*
@@ -52,6 +62,9 @@ void setup() {
 #ifdef MY_DEBUG
   Serial.println("setup");
 #endif
+  // Make sure that ATSHA204 is not floating
+  pinMode(ATSHA204_PIN, INPUT);
+  digitalWrite(ATSHA204_PIN, HIGH);
   // Reset unused pins
   int count = sizeof(unusedPins)/sizeof(int);
   for (int i = 0; i < count; i++) {
@@ -99,7 +112,17 @@ void sendValues() {
     oldBatPercentage = perc;
   }
   // Send other sensor values
-  // ...
+  si7021_thc data = siSensor.getTempAndRH();
+  float temperature = data.celsiusHundredths / 100.0;
+  int humidity = data.humidityPercent;
+#ifdef MY_DEBUG
+  Serial.print("T: ");
+  Serial.println(temperature);
+  Serial.print("H: ");
+  Serial.println(humidity);
+#endif
+  send(msgTemp.set(temperature, 1));
+  send(msgHum.set(humidity));
 }
 
 /*
